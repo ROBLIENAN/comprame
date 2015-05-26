f785f44be0fb3ed1890b8bf0e84755aeb713ad24
roberto lienan <master_r16@hotmail.com>
2015-05-26 19:07:58 -0300

    respaldo

--- config/initializers/omniouth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, " IvSPgRHaCCuHiMKxOwrliQs33", "gFjKXnfqciYSsEU935BCzOEaLH0mCaYf6CGjorE4hFrIACpguT"
-  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
-end
